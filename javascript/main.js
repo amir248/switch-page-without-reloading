5400fa921f14b2d475a08ac0361826a988b160af
chikchilcy <chikchicly@gmail.com>
2024-08-30 09:51:13 +0500

    new Whatsapp

--- javascript/main.js
+++ javascript/main.js
@@ -36,7 +36,7 @@ function domcontentloaded(){
                         document.querySelector('#content').innerHTML=`Two page! <br> This order site with SEO and adveritser. <br> The specialization of the company "Baron Sajterstauzen Web Workshop" is creating websites using simple JavaScript technologies with SEO optimization and promotion in contextual advertising.`;
                     }else if(window.location.hash=='#contacts'){
                         console.log('Three');
-                        document.querySelector('#content').innerHTML=`Three page! <br>  This contacts <br> My WhatsApp <a href="https://wa.clck.bar/79528885656">+79528885656</a> And my phone number <a href="tel:+77004145901">+77004145901</a>`;
+                        document.querySelector('#content').innerHTML=`Three page! <br>  This contacts <br> My WhatsApp <a href="https://wa.clck.bar/77004145901">+77004145901</a> And my phone number <a href="tel:+77004145901">+77004145901</a>`;
                     }else if(window.location.href=='http://localhost:3000/'||'https://amir248.github.io/switch-page-without-reloading/'){
                         console.log('^_^');
                         setTimeout(()=>{
