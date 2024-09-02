1a72cc9c4c1e19e879f1e82a42a0caf739b72ead
chikchilcy <chikchicly@gmail.com>
2024-09-02 12:02:49 +0500

    //

--- javascript/main.js
+++ javascript/main.js
@@ -38,7 +38,7 @@ function domcontentloaded(){
                         console.log('Three');
                         document.querySelector('#content').innerHTML=`Three page! <br>  This contacts <br> My WhatsApp <a href="https://wa.clck.bar/77004145901">+77004145901</a> And my phone number <a href="tel:+77004145901">+77004145901</a>`;
                     }else if(window.location.href=='http://localhost:3000/'||'https://amir248.github.io/switch-page-without-reloading/'){
-                        console.log('^_^');
+                        // console.log('^_^');
                         setTimeout(()=>{
                             document.querySelector('#content').innerHTML=`Home page! <br>
                                This is a simple example of a site with loading pages via javascript, like this: "single page application". So that you can make a multi-page site and push it to the "free" GitHub, with switching pages where there is a loading of sections.
