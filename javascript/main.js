ea866a2e68cd153e4395f5e65703cf40866e1f47
chikchilcy <chikchicly@gmail.com>
2024-09-03 09:03:07 +0500

    oK

--- javascript/main.js
+++ javascript/main.js
@@ -42,8 +42,19 @@ function domcontentloaded(){
                         setTimeout(()=>{
                             document.querySelector('#content').innerHTML=`Home page! <br>
                                This is a simple example of a site with loading pages via javascript, like this: "single page application". So that you can make a multi-page site and push it to the "free" GitHub, with switching pages where there is a loading of sections.
+                               <div class="container"><h1 id="animated-text">Привет, мир! It's animation text here</h1></div>
                             `;
                         },1700);
+                        setTimeout(()=>{
+                            let textAnimation=document.createElement('link');
+                            textAnimation.setAttribute('rel','stylesheet');
+                            textAnimation.href='style/text-animation.css';
+                            document.querySelector('head').append(textAnimation);
+                            let textAnimationJS=document.createElement('script');
+                            textAnimationJS.src='javascript/text-animation.js';
+                            document.querySelector('body').append(textAnimationJS)
+
+                        },1777);
 
                     }else{
                         console.log('else');
