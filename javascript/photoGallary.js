ba2d9a9e8a9423936f9a7ddcf3aa4882c2296924
chikchilcy <chikchicly@gmail.com>
2024-09-16 20:42:56 +0500

    hi setAttirbute

--- javascript/photoGallary.js
+++ javascript/photoGallary.js
@@ -3,16 +3,20 @@ img.setAttribute('src','https://qucu.ru/img/correctPhotoGallary/DSC_8196.webp');
 img.setAttribute('id','nextPhoto');
 document.querySelector('picture').append(img);
 
-let countClick=+0;
+let countClick=+1;
 document.querySelector('#nextPhoto').addEventListener("click",()=>{
     countClick++;
     // console.log('oKK '+ countClick);
     img.src='https://amir.qucu.ru/public/images/DSC_7774-550.webp';
     if(countClick==2){
-        // countClick=0;
-        img.src='https://qucu.ru/img/correctPhotoGallary/DSC_8196.webp';
+        img.src='https://qucu.ru/blozhik/img/beg/P11111-132550.jpg';
+    console.log('it\'s count Click '+countClick);
     }else if(countClick==3){
-        img.src='https://qucu.ru/blozhik/img/excellent-work/DSC_8232-900.webp';
-        countClick=0;
+    countClick=0;
+    img.src='https://qucu.ru/blozhik/img/excellent-work/DSC_8232-900.webp';
+    console.log('it\'s count Click '+countClick);
+    }else{
+        console.log('IF ELSE '+countClick);
+        return img.setAttribute('src','https://qucu.ru/img/correctPhotoGallary/DSC_8196.webp');
     }
 });
