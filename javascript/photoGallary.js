4adf16246b9c102ade1f999e53c88580cea7ae98
chikchilcy <chikchicly@gmail.com>
2024-09-17 16:46:25 +0500

    Photo gallary new file

--- javascript/photoGallary.js
+++ javascript/photoGallary.js
@@ -1,22 +1,72 @@
 const img=document.createElement('img');
-img.setAttribute('src','https://qucu.ru/img/correctPhotoGallary/DSC_8196.webp');
+img.setAttribute('src','img/1726571948298.jpg');
 img.setAttribute('id','nextPhoto');
 document.querySelector('picture').append(img);
 
-let countClick=+1;
+var countClick=+1;
 document.querySelector('#nextPhoto').addEventListener("click",()=>{
     countClick++;
     // console.log('oKK '+ countClick);
-    img.src='https://amir.qucu.ru/public/images/DSC_7774-550.webp';
+    img.src='img/1726571948249.jpg';
     if(countClick==2){
-        img.src='https://qucu.ru/blozhik/img/beg/P11111-132550.jpg';
+        img.src='img/1726572642360.jpg';
     console.log('it\'s count Click '+countClick);
     }else if(countClick==3){
-    countClick=0;
-    img.src='https://qucu.ru/blozhik/img/excellent-work/DSC_8232-900.webp';
+    img.src='img/1726571948265.jpg';
     console.log('it\'s count Click '+countClick);
-    }else{
+    }else if(countClick==4){
+        img.src='img/1726571948284.jpg';
+    countClick=0;
+}else{
         console.log('IF ELSE '+countClick);
-        return img.setAttribute('src','https://qucu.ru/img/correctPhotoGallary/DSC_8196.webp');
+        return img.setAttribute('src','img/1726571948298.jpg');
     }
 });
+let leftButton=document.createElement('span');
+leftButton.setAttribute('id','leftButton');
+leftButton.innerHTML='&lt;';
+leftButton.style.cssText='position:absolute;font-size:33px;text-shadow: 1px 1px white;left:10px;border-radius:50%; background:rgba(255,255,255,0.3);';
+document.querySelector('picture').prepend(leftButton);
+let rightButton=document.createElement('span');
+rightButton.setAttribute('id','rightButton'); 
+rightButton.innerHTML='&gt;';
+rightButton.style.cssText='position:absolute;font-size:33px;text-shadow: 1px 1px white;right:10px;border-radius:50%; background:rgba(255,255,255,0.3);';
+document.querySelector('picture').append(rightButton);
+setTimeout(()=>{
+    document.querySelector('#leftButton').addEventListener('click',()=>{
+            countClick++;
+            // console.log('oKK '+ countClick);
+            img.src='img/1726571948249.jpg';
+            if(countClick==2){
+                img.src='img/1726572642360.jpg';
+            console.log('it\'s count Click '+countClick);
+            }else if(countClick==3){
+            img.src='img/1726571948265.jpg';
+            console.log('it\'s count Click '+countClick);
+            }else if(countClick==4||countClick>4){
+                img.src='img/1726571948284.jpg';
+            countClick=0;
+        }else{
+                console.log('IF ELSE '+countClick);
+                return img.setAttribute('src','img/1726571948298.jpg');
+            }
+    });
+    document.querySelector('#rightButton').addEventListener('click',()=>{
+            countClick--;
+            // console.log('oKK '+ countClick);
+            img.src='img/1726571948249.jpg';
+            if(countClick==2){
+                img.src='img/1726572642360.jpg';
+            console.log('it\'s count Click '+countClick);
+            }else if(countClick==3){
+            img.src='img/1726571948265.jpg';
+            console.log('it\'s count Click '+countClick);
+            }else if(countClick==0){
+                img.src='img/1726571948284.jpg';
+            countClick=4;
+        }else{
+                console.log('IF ELSE '+countClick);
+                return img.setAttribute('src','img/1726571948298.jpg');
+            }
+    });
+},777);
